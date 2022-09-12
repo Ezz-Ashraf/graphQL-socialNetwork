034c682a9c6667b61d74fd7bcb5bdac5f4195e42
Ezz-Ashraf <ezzashraf056@gmail.com>
2022-09-12 20:59:13 +0200

    test mutation

--- graphql/schema.js
+++ graphql/schema.js
@@ -2,18 +2,38 @@ const { buildSchema } = require('graphql')
 //use backticks for multiLine string
 module.exports = buildSchema(`
 
-    type testData {
-        text: String
-        views: Int
-    }
-  
-  
-    type RootQuery {
-        hello :testData
+
+    type post {
+        _id :ID!
+        content:String!
+        title:String!
+        imageURL:String!
+        createdAt:String!
+        updatedAt:String!
     }
 
+    type User {
+        _id:ID!
+        email:String!
+        name:String!
+        password:String
+        status: String!
+        posts:[post!]! 
+    }
+
+    input userInputData {
+        email:String!
+        name:String!
+        password:String!
+    } 
+
+
+   type RootMutation {
+    createUser(userInput:userInputData)
+   }
+
     schema {
-        query : RootQuery
+        mutation : RootMutation
     }
 
 
